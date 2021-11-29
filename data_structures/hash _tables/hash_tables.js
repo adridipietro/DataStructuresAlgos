28d40a904ce9f125f9f73be3a23fffc28de6ef49
Adriana <am20dipi@alum.siena.edu>
2021-11-29 12:33:05 -0500

    Writing out a class HashTable in hash_table.js; also building out a simple hash function that generates a hash given a key

--- data_structures/hash _tables/hash_tables.js
+++ data_structures/hash _tables/hash_tables.js
@@ -15,4 +15,26 @@ user.saysBye('Bye') // O(1)
 
 
 
+// Example hash table with Class syntax
 
+class HashTable {
+    // constructor receives a "size"
+    constructor(size){
+        // instantiate a new instance of Array
+        // this array is going to hold the data for us
+        this.data = new Array(size)
+    }
+
+    // hash function -- you need hash function to create hash table
+    // this is going to generate a hash for us
+    _hash(key){
+        let hash = 0
+        for (let i = 0; i < key.length; i++){
+            hash = (hash + key.charCodeAt(i) * i) % this.data.length
+        }
+        return hash
+    }
+}
+
+// given the same input, hash tables will always output the same data
+// data lives in "buckets"
