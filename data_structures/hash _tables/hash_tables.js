4aa75aaf716d51d270796c8abfc352de98062193
Adriana <am20dipi@alum.siena.edu>
2021-11-29 12:44:01 -0500

    Building out set method for hash table; pushing key into hash table at specific address

--- data_structures/hash _tables/hash_tables.js
+++ data_structures/hash _tables/hash_tables.js
@@ -28,13 +28,36 @@ class HashTable {
     // hash function -- you need hash function to create hash table
     // this is going to generate a hash for us
     _hash(key){
+        // declare + assign variable "hash" to 0
         let hash = 0
+        // loop through the key; loops through each character of the key
         for (let i = 0; i < key.length; i++){
+            // reassign hash to equal "hash (0)" + random integer multiplied by the index
+            // and then use modular operator to make sure this hash remains in our data length.
             hash = (hash + key.charCodeAt(i) * i) % this.data.length
         }
         return hash
     }
+
+    // set method 
+    set(key, value){
+        // address reps where we want to store this info
+        let address = this._hash(key)
+        if (!this.data[address]){
+            this.data[address] = []
+            this.data[address].push([key, value])
+            console.log(this.data)
+        } 
+        this.data[address].push([key, value])
+    }
 }
 
+const myHashTable = new HashTable(50)
+// 50 reps the amount of space we have to store different hashes/addresses
+
+
+
+
+// _hash => the underscore just tells us that this function or method is "private" -- its scope is only within the class function
 // given the same input, hash tables will always output the same data
 // data lives in "buckets"
