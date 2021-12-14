f2c5be86f37546f061af6cb9c55881b613ca8cf5
Adriana <am20dipi@alum.siena.edu>
2021-12-13 19:55:44 -0500

    Building out constructor, prepend() and append() in class DoubleLinkedList

--- data_structures/linked_lists/doubleLinkedList.js
+++ data_structures/linked_lists/doubleLinkedList.js
@@ -7,6 +7,48 @@
 
 
 class DoubleLinkedList {
+    constructor(value){
+        this.head = {
+            value: value,
+            next: null,
+            prev: null
+        }
+    }
+    append(value){
+        const newNode = {
+            value: value,
+            next: null,
+            prev: null
+        }
+        newNode.prev = this.tail
+        this.tail.next = newNode
+        this.tail = newNode
+        this.length++
+        return this
 
+    }
+    prepend(value){
+        const newNode = {
+            value: value,
+            next: null,
+            prev: null
+        }
+        newNode.next = this.head
+        this.head.prev = newNode
+        this.head = newNode
+        this.length++
+        return this
+
+    }
+    insert(index, value){
+        
+    }
+    remove(index){
+
+    }
 }
 
+
+const myDouble = new DoubleLinkedList(10)
+myDouble.append(1)
+myDouble.prepend(33)
