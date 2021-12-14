25f7e75de33c981c4565c80342262a7c86ef6b81
Adriana <am20dipi@alum.siena.edu>
2021-12-13 19:50:49 -0500

    Creating doubleLinkedList.js file; creating class DoubleLinkedList; adding notes on double linked lists on top of file

---
+++ data_structures/linked_lists/doubleLinkedList.js
@@ -0,0 +1,12 @@
+// DOUBLE LINKED LISTS
+    // points to both the node before and after it
+    // while a single linked list only points to the next node
+
+    // double => allows us to traverse the list backwards, as well as forwards
+    // double linked lists require more memory
+
+
+class DoubleLinkedList {
+
+}
+
