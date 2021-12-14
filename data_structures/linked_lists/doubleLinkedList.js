a0a31d3573e10661f66433840940d9ca05a109b6
Adriana <am20dipi@alum.siena.edu>
2021-12-13 20:09:09 -0500

    Creating traverseToIndex(), printList() and insert() in class DoubleLinkedList

--- data_structures/linked_lists/doubleLinkedList.js
+++ data_structures/linked_lists/doubleLinkedList.js
@@ -40,8 +40,51 @@ class DoubleLinkedList {
         return this
 
     }
+    traverseToIndex(index){
+        let counter = 0
+        let currentNode = this.head
+        // keep traversing until the counter is equal to the index
+        while (counter !== index){
+            // keep moving the currentNode to the right
+            currentNode = currentNode.next
+            // increment the counter
+            counter++
+        }
+        return currentNode
+
+    }
+    // print value of nodes in an array format
+    printList(){
+        // create empty array
+        const array = []
+        // set currentNode to the first item in LinkedList (the head)
+        let currentNode = this.head
+        // create a while loop; while the currentNode does not equal null do this:
+        while (currentNode !== null){
+            // push currentNode's value to the array
+            array.push(currentNode.value)
+            // and then set the currentNode to the property of next
+            currentNode = currentNode.next
+        }
+        return array
+    }
     insert(index, value){
-        
+        if ( index >= this.length){
+            return this.append(value)
+        }
+        const newNode = {
+            value: value,
+            next: null,
+            prev: null
+        }
+        const leader = this.traverseToIndex(index - 1)
+        const follower = leader.next
+        leader.next = newNode
+        newNode.prev = leader
+        newNode.next = follower
+        follower.prev = newNode
+        this.length++
+
     }
     remove(index){
 
@@ -52,3 +95,5 @@ class DoubleLinkedList {
 const myDouble = new DoubleLinkedList(10)
 myDouble.append(1)
 myDouble.prepend(33)
+myDouble.insert(3, 12)
+
