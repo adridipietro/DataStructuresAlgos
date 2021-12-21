0e472d629bd849b460277a0433f1bc681bb33e2c
Adriana <am20dipi@alum.siena.edu>
2021-12-21 11:29:04 -0500

    Adding notes to stackLinkedList.js; renamed stack.js to stackLinkedList; creating stackArray.js

--- data_structures/stacks_queues/stackLinkedList.js
+++ data_structures/stacks_queues/stackLinkedList.js
@@ -20,23 +20,33 @@ class Stack {
     push(value){
         // create a const newNode; instantiate a new instance of class Node -- passing in the value
         const newNode = new Node(value)
+        // if the stack is empty, the new node added is both top + bottom
         if (this.length === 0){
             this.top = newNode
             this.bottom = newNode
+        // otherwise, set this.top to the newNode and set the previous top node to the current top node's next value
         } else {
             const holdingPointer = this.top
             this.top = newNode
             this.top.next = holdingPointer
         }
+        // increment length + return this (the stack)
         this.length++
         return this
         
     }
     // remove the top node of the stack
     pop(){
+        // if the top node does not exist; meaning if the stack is empty, return null
         if (!this.top){
             return null
         }
+        // if the stack only has one item (top === bottom), in addition to removing the top, set the bottom to null
+        if (this.top === this.bottom){
+            this.bottom = null
+        }
+        // the top now becomes the previous tops' next value
+        // decrement length and return this ( the stack )
         this.top = this.top.next
         this.length--
         return this
