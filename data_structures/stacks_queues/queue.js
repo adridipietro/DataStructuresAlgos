c12d4df251b13c79168296d3c69ce6a5426c9b8d
Adriana <am20dipi@alum.siena.edu>
2021-12-21 11:37:54 -0500

    Creating file queue.js; building out class Node; building out Class Stack with constructor(), peek() and enqueue() and dequeue() outline

---
+++ data_structures/stacks_queues/queue.js
@@ -0,0 +1,28 @@
+// Building a QUEUE using a LINKED LIST
+class Node {
+    constructor(value){
+        this.value = value
+        this.next = null
+    }
+}
+
+class Stack {
+    constructor(){
+        this.first = null
+        this.last = null
+        this.length = 0
+    }
+    // access first element of the queue
+    peek(){
+        return this.first
+    }
+    // add to the end of the queue
+    enqueue(value){
+
+    }
+    // remove the first element of the queue
+    dequeue(){
+
+    }
+
+}
