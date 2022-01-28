a42cef987e4324bc4186aecdcf80dfd1eb58d367
Adriana <am20dipi@alum.siena.edu>
2022-01-27 21:57:02 -0500

    Creating new file longestSubstring.js; plotting out pseudocode for solving this algo challenge; including instructions, input and output

---
+++ data_structures/arrays/longestSubstring.js
@@ -0,0 +1,21 @@
+// Given a string 's', find the length of the longest substring without repeating characters.
+
+// input = string
+// output = integer
+
+function lengthOfLongestSubstring(s){
+    // GOALS
+    // if length of string === 0, return 0
+    // if length of string === 1, return 1
+    // declare temp variable
+    // declare length variable
+    // loop through the string
+        // if the index of the letter of the string === the last index of the letter 
+            // slice it off
+        // else 
+            // push the letter into a new temp var
+            // reassign the length variable
+            // if the length variable of the substring is larger than the current substring...
+    // return the length
+
+}
