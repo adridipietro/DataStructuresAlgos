d51aba16a8f084de9f1584723e95a74941ad8d00
Adriana <am20dipi@alum.siena.edu>
2022-02-08 10:29:52 -0500

    Coding out another solution for removeElement.js

--- data_structures/arrays/removeElement.js
+++ data_structures/arrays/removeElement.js
@@ -27,3 +27,22 @@ var removeElement = function(array, val) {
 }
 
 console.log(removeElement([3,2,3], 3))
+
+
+// Other Solution
+
+var removeElement2 = function(array, val) {
+     // create a 'for' loop to iterate through the array
+     for (let i = 0; i < array.length; i++){
+      // set a variable 'item' to each array item
+      let item = array[i]
+      // if the array item's value is equal to 'val'
+      if (array[i] === val) {
+          // splice that value at the index
+          array.splice(i, 1);
+          // decrement i
+          i--
+      }
+    }
+  return array.length
+}
